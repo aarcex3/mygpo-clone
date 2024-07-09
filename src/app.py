c3a723f41ae262dfd933c50b179f68a5e362f2e2
Agustin Arce <agustinarceprincich@gmail.com>
2024-07-09 17:29:06 -0400

    Initial Commit

---
+++ src/app.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI(title="Gpodder.net clone", version="0.1.0")
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
