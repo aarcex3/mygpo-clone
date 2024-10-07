774f81a4356ff2b1c5aea36fea5ca21de68315a4
Agustin Arce <agustinarceprincich@gmail.com>
2024-10-07 12:07:10 -0300

    refactor (testing): refactor auth tests

--- test/auth_test.go
+++ test/auth_test.go
@@ -21,51 +21,49 @@ func performFormRequest(app *gin.Engine, method, path string, form url.Values) *
 	return res
 }
 
-func TestRegistration(t *testing.T) {
-	t.Run("Normal registration", func(t *testing.T) {
-		app, db, cleanup := testconfig.SetupAppWithDB()
-		defer cleanup()
+func TestRegistrationNormal(t *testing.T) {
+	app, db, cleanup := testconfig.SetupAppWithDB()
+	defer cleanup()
 
-		form := url.Values{
-			"username": {"johndoe"},
-			"email":    {"john@example.com"},
-			"password": {"supersecretpassword"},
-		}
+	form := url.Values{
+		"username": {"johndoe"},
+		"email":    {"john@example.com"},
+		"password": {"supersecretpassword"},
+	}
 
-		res := performFormRequest(app, "POST", "/v1/auth/registration", form)
-		assert.Equal(t, http.StatusCreated, res.Code)
-		assert.JSONEq(t, `{"message":"Registration successful"}`, res.Body.String())
+	res := performFormRequest(app, "POST", "/v1/auth/registration", form)
+	assert.Equal(t, http.StatusCreated, res.Code)
+	assert.JSONEq(t, `{"message":"Registration successful"}`, res.Body.String())
 
-		var count int
-		err := db.QueryRow("SELECT COUNT(*) FROM users WHERE username = ?", "johndoe").Scan(&count)
-		assert.NoError(t, err)
-		assert.Equal(t, 1, count)
-	})
+	var count int
+	err := db.QueryRow("SELECT COUNT(*) FROM users WHERE username = ?", "johndoe").Scan(&count)
+	assert.NoError(t, err)
+	assert.Equal(t, 1, count)
 
-	t.Run("Try to register an existing user", func(t *testing.T) {
-		app, _, cleanup := testconfig.SetupAppWithDB()
-		defer cleanup()
-
-		form := url.Values{
-			"username": {"johndoe"},
-			"email":    {"john@example.com"},
-			"password": {"supersecretpassword"},
-		}
-
-		_ = performFormRequest(app, "POST", "/v1/auth/registration", form)
-
-		// Try registering the same user again
-		res := performFormRequest(app, "POST", "/v1/auth/registration", form)
-		assert.Equal(t, http.StatusBadRequest, res.Code)
-		assert.JSONEq(t, `{"message":"User already exists"}`, res.Body.String())
-	})
 }
 
-func TestLogin(t *testing.T) {
+func TestRegistrationUserExists(t *testing.T) {
 	app, _, cleanup := testconfig.SetupAppWithDB()
 	defer cleanup()
 
-	// Register the user first
+	form := url.Values{
+		"username": {"johndoe"},
+		"email":    {"john@example.com"},
+		"password": {"supersecretpassword"},
+	}
+
+	_ = performFormRequest(app, "POST", "/v1/auth/registration", form)
+
+	// Try registering the same user again
+	res := performFormRequest(app, "POST", "/v1/auth/registration", form)
+	assert.Equal(t, http.StatusBadRequest, res.Code)
+	assert.JSONEq(t, `{"message":"User already exists"}`, res.Body.String())
+}
+
+func TestLoginSuccess(t *testing.T) {
+	app, _, cleanup := testconfig.SetupAppWithDB()
+	defer cleanup()
+
 	registrationForm := url.Values{
 		"username": {"johndoe"},
 		"email":    {"john@example.com"},
@@ -73,46 +71,66 @@ func TestLogin(t *testing.T) {
 	}
 	_ = performFormRequest(app, "POST", "/v1/auth/registration", registrationForm)
 
-	t.Run("Successful login", func(t *testing.T) {
-		form := url.Values{
-			"username": {"johndoe"},
-			"password": {"supersecretpassword"},
-		}
-		res := performFormRequest(app, "POST", "/v1/auth/login", form)
+	form := url.Values{
+		"username": {"johndoe"},
+		"password": {"supersecretpassword"},
+	}
+	res := performFormRequest(app, "POST", "/v1/auth/login", form)
 
-		assert.Equal(t, http.StatusOK, res.Code)
-		assert.JSONEq(t, `{"message": "Login successful"}`, res.Body.String())
+	assert.Equal(t, http.StatusOK, res.Code)
+	assert.JSONEq(t, `{"message": "Login successful"}`, res.Body.String())
 
-		authHeader := res.Header().Get("Authorization")
-		assert.NotEmpty(t, authHeader)
-		assert.Contains(t, authHeader, "Bearer")
-	})
+	authHeader := res.Header().Get("Authorization")
+	assert.NotEmpty(t, authHeader)
+	assert.Contains(t, authHeader, "Bearer")
 
-	t.Run("Login with wrong username", func(t *testing.T) {
-		form := url.Values{
-			"username": {"johndo"},
-			"password": {"supersecretpassword"},
-		}
-		res := performFormRequest(app, "POST", "/v1/auth/login", form)
+}
 
-		assert.Equal(t, http.StatusUnauthorized, res.Code)
-		assert.JSONEq(t, `{"message": "Login error"}`, res.Body.String())
+func TestLoginWrongUsername(t *testing.T) {
+	app, _, cleanup := testconfig.SetupAppWithDB()
+	defer cleanup()
 
-		authHeader := res.Header().Get("Authorization")
-		assert.Empty(t, authHeader)
-	})
+	registrationForm := url.Values{
+		"username": {"johndoe"},
+		"email":    {"john@example.com"},
+		"password": {"supersecretpassword"},
+	}
+	_ = performFormRequest(app, "POST", "/v1/auth/registration", registrationForm)
 
-	t.Run("Login with wrong password", func(t *testing.T) {
-		form := url.Values{
-			"username": {"johndoe"},
-			"password": {"notasecretpassword"},
-		}
-		res := performFormRequest(app, "POST", "/v1/auth/login", form)
+	form := url.Values{
+		"username": {"johndo"},
+		"password": {"supersecretpassword"},
+	}
+	res := performFormRequest(app, "POST", "/v1/auth/login", form)
 
-		assert.Equal(t, http.StatusUnauthorized, res.Code)
-		assert.JSONEq(t, `{"message": "Login error"}`, res.Body.String())
+	assert.Equal(t, http.StatusUnauthorized, res.Code)
+	assert.JSONEq(t, `{"message": "Login error"}`, res.Body.String())
 
-		authHeader := res.Header().Get("Authorization")
-		assert.Empty(t, authHeader)
-	})
+	authHeader := res.Header().Get("Authorization")
+	assert.Empty(t, authHeader)
+
+}
+
+func TestLoginWrongPassword(t *testing.T) {
+	app, _, cleanup := testconfig.SetupAppWithDB()
+	defer cleanup()
+
+	registrationForm := url.Values{
+		"username": {"johndoe"},
+		"email":    {"john@example.com"},
+		"password": {"supersecretpassword"},
+	}
+	_ = performFormRequest(app, "POST", "/v1/auth/registration", registrationForm)
+
+	form := url.Values{
+		"username": {"johndoe"},
+		"password": {"notasecretpassword"},
+	}
+	res := performFormRequest(app, "POST", "/v1/auth/login", form)
+
+	assert.Equal(t, http.StatusUnauthorized, res.Code)
+	assert.JSONEq(t, `{"message": "Login error"}`, res.Body.String())
+
+	authHeader := res.Header().Get("Authorization")
+	assert.Empty(t, authHeader)
 }
